5738fb246fc95cd611581290e0f0cf0bd36acb99
Iamkurokiji <grantlimsaputra@gmail.com>
2025-06-10 18:27:01 +0700

    The projectiles can now disappear upon going out of boundary
    Yea no need description for this one lol

--- Assets/Apps/Scripts/Projectile.cs
+++ Assets/Apps/Scripts/Projectile.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float moveSpeed;
+
+    private float upperYBound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float zDist = transform.position.z - Camera.main.transform.position.z;
+
+        Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, zDist));
+        upperYBound = topRight.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+        // Destroy if it goes past the top screen boundary
+        if(transform.position.y >= upperYBound)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
